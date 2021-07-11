16e0a8019f7b1b6704a847976116616494660716
rzbsys <rzbsys@gmail.com>
2021-07-11 20:08:04 +0900

    front 최종 완성

--- js/script.js
+++ js/script.js
@@ -4,43 +4,69 @@ function reset() {
     $('#User-inform').hide();  
     $('#Main').hide();
     $('#Map').hide();
+    $('#Box').hide();
 }
 
 
-reset();;
-$('#Login').show();
+reset();
+$('#Map2').hide();
+Login();
 
 
-$('#F1').on('click', function() {
-    reset();
-    $('#Login').show();
-});
-
-$('#F2').on('click', function() {
-    reset();
-    $('#Main').show();
-});
-
-$('#F3').on('click', function() {
-    reset();
-    $('#Map').show();
-});
-
-$('#Logout').on('click', function() {
-    reset();
-    $('#Login').show();         
-    $('.bottom-bar').css('bottom', '');
-    $('.background').css('height', '');
-    $('.title').css('text-align', '');
-});
 
 
 $('#Lo-btn').on('click', function() {
-    reset();
-    $('#Main').show();      
-    $('.background').css('height', '78%');
+    Main();
+});
+$('.t-dot').on('click', function() {
+    Menu();
+});
+$('#Back').on('click', function() {
+    Main();
+});
+$('#Logout').on('click', function() {
+    Login();
+});
+$('#Record').on('click', function() {
+    alert('미구현');
+});
+$('#Maps').on('click', function() {
+    Maps();
+});
+$('.box').on('click', function() {
+    Open(this);
+});
+$('.close').on('click', function() {
+    $('#Box').hide();
 });
 
+
+function Open(e) {
+    $('#Box').show();
+}
+
+function Maps() {
+    reset();
+    $('.background').css('height', '80%');
+    $('#Map').show();
+}
+function Login() {
+    reset();
+    $('.background').css('height', '');
+    $('#Login').show();
+}
+
+function Main() {
+    reset();
+    $('#User').show();  
+    $('.background').css('height', '80%');
+}
+
+function Menu() {
+    reset();
+    $('#Main').show();      
+    $('.background').css('height', '80%');
+}
 
 function select(e) {
     console.log(e.value);
